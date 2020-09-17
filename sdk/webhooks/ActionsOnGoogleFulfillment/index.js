38687bad39daa399b07bd34cf44ef96921203eb2
chatasweetie <jearleycha@google.com>
2020-09-16 17:38:13 -0700

    add undefined to lastSeenTime
    b/168748146
    
    Change-Id: Icc5e884bf5f9eabc0e7a78d2082b466b88a8d09e

--- sdk/webhooks/ActionsOnGoogleFulfillment/index.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -23,9 +23,9 @@ const optionsNeedA = new Set();
 optionsNeedA.add('horse').add('phone');
 
 app.handle('greeting', conv => {
-  let message = 'A wondrous greeting, adventurer! Welcome to the mythical land of  Gryffinberg! Based on your clothes, you are not from around these lands. It looks like you\'re on your way to an epic journey.';
-  if (conv.user.lastSeenTime) {
-    message = 'A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!';
+  let message = 'A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!';
+  if (!conv.user.lastSeenTime) {
+    message = 'Welcome to the mythical land of  Gryffinberg! Based on your clothes, you are not from around these lands. It looks like you\'re on your way to an epic journey.';
   }
   conv.add(message);
 });
